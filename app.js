63a5f8f02ffa0cab3d0d3da2e8a3503271862d48
willow <willowfrazey@gmail.com>
2023-11-17 19:14:23 -0800

    Fixed some stuff and added stuff too!

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Import required modules and configurations
 import "dotenv/config";
 import express from "express";
 import path from "path";
@@ -5,6 +6,23 @@ import cookieParser from "cookie-parser";
 import logger from "morgan";
 import sessions from "express-session";
 
-import models from "./models.js";
+// Import custom modules for database models, API routes, and authentication
+import models from "./models/models.js";
 import apiRouter from "./routes/api/api.js";
-import authProvider from "./auth.js";
+import authProvider from "./models/auth.js";
+
+// Initialize the Express app
+const app = express();
+
+// Setting up middleware
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static("public"));
+
+// Middleware to attach the models to every request
+app.use((req, res, next) => {
+  req.models = models;
+  next();
+});
